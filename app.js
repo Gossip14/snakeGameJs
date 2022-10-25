0c172b455cbc7c891fc707eb05933a8c3ffdbf31
Patrick Jordan <patrick.djivotsane@student.uni-siegen.de>
2022-10-25 20:53:05 +0200

    Das Spiel wurde weiterentwickelt und funktioniert schon. Aber natürlich muss es verbessert werden

--- app.js
+++ app.js
@@ -13,10 +13,14 @@ var snakeY=blockSize*5;
 var velocityX=0;
 var velocityY=0;
 
+var snakeBody=[];
+
 
 //food
 var foodX;
 var foodY;
+
+var gameOver = false;
 
 window.onload=function(){
 	board=document.getElementById("board");
@@ -33,33 +37,70 @@ window.onload=function(){
 }
 
 function update() {
+
+	if(gameOver){
+		return;// stop drawing when it is gameover
+	}
 	// Draw the canvas
 	context.fillStyle="black";
 	context.fillRect(0,0,board.width,board.height);
+
+	 // Draw the food
+	context.fillStyle="red";
+	context.fillRect(foodX,foodY,blockSize,blockSize);
+
+	if(snakeX==foodX && snakeY==foodY){
+		snakeBody.push([foodX,foodY]);
+		placeFood();
+	}
+
+	for (let i=snakeBody.length-1;i > 0;i--){
+		snakeBody[i]= snakeBody[i-1];//we tell the tail that he shoul take the previous place of the head
+	}
+
+	if(snakeBody.length){
+		snakeBody[0]=[snakeX,snakeY];
+	}
     //Draw th snake
 	context.fillStyle="lime";
 	snakeX+=velocityX*blockSize;
 	snakeY+=velocityY*blockSize;
 	context.fillRect(snakeX,snakeY,blockSize,blockSize); 
-    // Draw the food
-	context.fillStyle="red";
-	context.fillRect(foodX,foodY,blockSize,blockSize);
+	for (let i=0;i<snakeBody.length;i++){
+		context.fillRect(snakeBody[i][0],snakeBody[i][1],blockSize,blockSize);
+
+	}
+
+		// game over conditions
+		if(snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize){
+			gameOver=true;
+			alert("Game Over");
+		}
+
+		for (let i=0; i<snakeBody.length;i++){
+			if(snakeX == snakeBody[i][0] && snakeY == SnakeBody[i][1]){
+			gameOver=true;
+			alert("Game Over");
+		  }
+		}
+	
+   
 }
 
 function changeDirection(e){
-	if(e.code=="ArrowUp"){
+	if(e.code=="ArrowUp" && velocityY!= 1){
 		velocityX=0;
 		velocityY=-1;
 	}
-	else if(e.code=="ArrowDown"){
+	else if(e.code=="ArrowDown" && velocityY!= -1){
 		velocityX=0;
 		velocityY=1;
 	}
-	if(e.code=="ArrowLeft"){ 
+	if(e.code=="ArrowLeft" && velocityX!= 1){ 
 		velocityX=-1;
 		velocityY=0;
 	}
-	if(e.code=="ArrowRight"){
+	if(e.code=="ArrowRight" && velocityX!= -1){
 		velocityX=1;
 		velocityY=0;
 	}
